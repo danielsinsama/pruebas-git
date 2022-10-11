d02eb9b43ebb670ff876d5c37e1021953cc0acd8
Daniel Ramirez Huayanay <dramirez@rashperu.com>
2022-10-10 23:40:13 -0500

    leyendod datos excel

---
+++ llenado.js
@@ -0,0 +1,21 @@
+const XLSX = require('xlsx')
+const fs = require('fs')
+let content = ''
+const leerExcel = (ruta) => {
+    const workBook = XLSX.readFile(ruta)
+    const workBookSheets = workBook.SheetNames;
+    const [sheet] = workBookSheets
+    const data = XLSX.utils.sheet_to_json(workBook.Sheets[sheet])
+    for (const itemFila of data){
+        console.log(itemFila);
+        content += `{ name:'${itemFila['name']}'},\n`
+    }
+    try {
+        const data = fs.writeFileSync('test.txt', content)
+        console.log('file written successfully')
+    } catch (err) {
+        console.error(err)
+    }
+}
+
+leerExcel('instrumentos.xlsx')
